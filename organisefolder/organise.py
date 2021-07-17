bd1810098280cb7e3692d916a84ea3e61677f25d
Mutaz Abu Ghazaleh <10687960+mutazag@users.noreply.github.com>
2021-07-17 10:56:52 +1000

    processing data frame and exract folder names

--- organisefolder/organise.py
+++ organisefolder/organise.py
@@ -6,6 +6,8 @@ from glob import glob
 from os import path
 import argparse
 import pandas as pd
+from datetime import datetime
+
 
 
 def main(args):
@@ -19,14 +21,36 @@ def main(args):
         files = glob(args.path)
         print(f'{len(files)} files found')
 
+
+    date_cols = ['year', 'month', 'day']
+
+    # create a dataframe from the files
     df = pd.DataFrame(files, columns=['file'])
     df['path'] = df['file'].apply(lambda x: path.dirname(x))
     df['name'] = df['file'].apply(lambda x: path.basename(x))
     df['ext'] = df['file'].apply(lambda x: path.splitext(x)[1])
     df['size'] = df['file'].apply(lambda x: path.getsize(x))
+    df['date'] = df['file'].apply(lambda x: datetime.utcfromtimestamp(path.getmtime(x)))
+    df['year'] = df.date.dt.year
+    df['month'] = df.date.dt.month
+    df['day'] =  df.date.dt.day
 
-    print(df)
-    print(df.describe())
+    df['destnation_folder'] = df.year.map(str) + '/' + df.month.map(str) + '/' + df.day.map(str)
+    df['dest_folder'] = df[date_cols].apply(lambda x: f'/{x.year:04}/{x.month:02}/{x.day:02}/', axis=1)
+
+
+
+    df_folders_dates = df.groupby(date_cols).file.count().reset_index().rename(columns={'file':'count'})
+    # print(df_folders_dates.head())
+
+    print('groupby')
+    for fldr, groupdf in df.groupby(['dest_folder']):
+        print(f'group:  {fldr}')
+        print(','.join(groupdf.name.to_list()))
+
+
+    # print(df.head())
+    # print(df.describe())
 
     print(len(df))
     pass
